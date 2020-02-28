eb3a8c34cf3ce607571bd7a9ff9c6f7a96c7ebd3
Pedro Ernesto Tramontina <pedro.tramontina@stwautomacao.com.br>
2020-02-28 10:57:36 -0300

    Added Fontawesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue'
 //Bulma
 import './assets/scss/main.scss'
 
+//FontAwesome
+import '@fortawesome/fontawesome-free/js/all'
+
 Vue.config.productionTip = false
 
 new Vue({
