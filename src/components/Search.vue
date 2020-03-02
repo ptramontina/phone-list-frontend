ff4e7de5122ba4c74002d419e2043d86f3d8500f
Pedro Ernesto Tramontina <pedro.tramontina@stwautomacao.com.br>
2020-03-02 11:48:42 -0300

    Add list button

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="field">
-    <p class="control has-icons-left">
-      <input class="input" type="text" placeholder="Type your search" />
-      <span class="icon is-left">
-        <i class="fas fa-search"></i>
-      </span>
-    </p>
+  <div class="columns">
+    <div class="column is-8">
+      <div class="field">
+        <p class="control has-icons-left">
+          <input class="input" type="text" placeholder="Type your search" />
+          <span class="icon is-left">
+            <i class="fas fa-search"></i>
+          </span>
+        </p>
+      </div>
+    </div>
+
+    <div class="column is-4">
+      <button class="button is-primary is-fullwidth">
+        <span class="icon">
+          <i class="far fa-address-book"></i>
+        </span>
+        <span>Phone list</span>
+      </button>
+    </div>
   </div>
 </template>
 
