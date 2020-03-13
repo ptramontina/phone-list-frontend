ddad7add07a952b84cdd94c2739c6690b28f044d
Pedro Ernesto Tramontina <pedro.tramontina@gmail.com>
2020-03-13 14:01:14 -0300

    Shows information about empty list

--- src/components/Phone.vue
+++ src/components/Phone.vue
@@ -6,10 +6,22 @@
         <p>{{data.company ? data.company.name : ''}}</p>
       </div>
       <div class="message-body">
-        <div class="level">
+        <div v-if="data.phones.length > 0" class="level">
           <div class="level-item" v-for="(item, index) in data.phones" :key="index">
             <phone-number :item="item"></phone-number>
           </div>
+        </div>
+        <div v-else class="level">
+          <div class="level-item">
+            <div class="field is-grouped is-grouped-multiline">
+              <div class="control">
+                <div class="tags has-addons">
+                  <span class="tag is-large"><i class="far fa-times-circle"></i></span>
+                  <span class="tag is-large">No phones found</span>
+                </div>
+              </div>
+            </div>                     
+          </div>                     
         </div>
       </div>
     </article>
